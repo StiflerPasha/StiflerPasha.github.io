2a70789f63b9430e6afeef66e95303756090de3f
StiflerPasha <dvoynospavel@gmail.com>
2019-05-12 15:01:25 +0300

    Initial commit

---
+++ precache-manifest.c953763e43ed11e2aa81dde262642321.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "85001c304e1be4980ff08126b45aba99",
+    "url": "/static/media/ava_post.85001c30.jfif"
+  },
+  {
+    "revision": "087aca2e835a8613c3d4159b049785c8",
+    "url": "/static/media/ava.087aca2e.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5ba58e01cf3b265bc64d",
+    "url": "/static/js/main.53da92b7.chunk.js"
+  },
+  {
+    "revision": "e2bcf207ce51504c19ee",
+    "url": "/static/js/2.a14a9b2b.chunk.js"
+  },
+  {
+    "revision": "5ba58e01cf3b265bc64d",
+    "url": "/static/css/main.7a38ad13.chunk.css"
+  },
+  {
+    "revision": "83ec79eef4899417689a37b6b569d49f",
+    "url": "/index.html"
+  }
+];
