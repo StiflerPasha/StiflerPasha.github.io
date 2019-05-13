f62608da103d7262ac143196129d320177e26145
StiflerPasha <dvoynospavel@gmail.com>
2019-05-13 20:23:40 +0300

    Initial commit

---
+++ precache-manifest.27beb0192574a484f707df6eea3acd3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf740a79569ff1bc101c4758e8b23283",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41b1610640f2164faae4",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "199d9596ae3e103242ae",
+    "url": "/static/css/main.462b0bd9.chunk.css"
+  },
+  {
+    "revision": "41b1610640f2164faae4",
+    "url": "/static/js/2.778cce55.chunk.js"
+  },
+  {
+    "revision": "199d9596ae3e103242ae",
+    "url": "/static/js/main.9bf6123b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
